5a710399dfd298e958d811cfd4d6202fd1999f82
matt <mpinner@gmail.com>
2012-11-05 12:17:55 -0800

    fixing multiple read buffer for mean measurements for each sensor

--- heineken/Arduino/_4_sensor4/_4_sensor4.ino
+++ heineken/Arduino/_4_sensor4/_4_sensor4.ino
@@ -1,6 +1,6 @@
-//Use of MaxSonar EZ1 sonar module in analog mode with arduino
+//Use of many syncronized MaxSonar sonar modules in analog mode with arduino
 
-int sensorCount = 4;
+const int sensorCount = 4;
 
 int rangePin[] = {0, 1, 2, 3};
 
@@ -8,10 +8,17 @@ int bwPin[] = {3, 4, 7};
 int bwCount = 3;
 
 //variables needed to store values
+const int arraysize = 5;  //quantity of values to find the median (sample size). Needs to be an odd number
+int rangevalue[sensorCount][arraysize] = 
+  {
+    {0, 0, 0, 0, 0},   
+    {0, 0, 0, 0, 0},   
+    {0, 0, 0, 0, 0},   
+    {0, 0, 0, 0, 0}   
+  };
 
 
-int arraysize = 5;  //quantity of values to find the median (sample size). Needs to be an odd number
-int rangevalue[] = {0, 0, 0, 0, 0};    //declare an array to store the samples. not necessary to zero the array values here, it just makes the code clearer
+//declare an array to store the samples. not necessary to zero the array values here, it just makes the code clearer
 //*********************************************************************************************
 void setup() 
 {
@@ -23,7 +30,7 @@ void setup()
 
   //This opens up a serial connection to shoot the results back to the PC console
   Serial.begin(9600);
-  printArray(rangevalue, arraysize);  
+  printArray(rangevalue[0], arraysize);  
   delay(5000);        //wait a while to open serial monitor window
 
 }
@@ -35,8 +42,9 @@ void loop()
   //MaxSonar Analog reads are known to be very sensitive. See the Arduino forum for more information.
   //Maxbotix does not recommend averaging readings as the occasional value can be wildly inaccurate and skew the average
   //A median sort is the preferred method. 
-
-   for(int i = 0; i < arraysize; i++)
+for ( int sensor = 0; sensor < sensorCount; sensor++) {
+  
+for(int i = 0; i < arraysize; i++)
    {                                                    //array pointers go from 0 to 4
 
     //Used to read in the analog voltage output that is being sent by the MaxSonar device.
@@ -45,7 +53,7 @@ void loop()
     //This yields a resolution between readings of: 5 volts / 1024 units or, .0049 volts (4.9 mV) per unit. 
     //Therefore, one unit from the arduino's ADC represents 0.5 inches
 
-     rangevalue[i] = analogRead(rangePin[0]);
+     rangevalue[0][i] = analogRead(rangePin[sensor]);
  /*
  Serial.print("i, value   ");
           Serial.print(i);
@@ -57,25 +65,25 @@ void loop()
     }  
 
  //  Serial.print("unsorted ");
-   printArray(rangevalue, arraysize);
+   printArray(rangevalue[sensor], arraysize);
 //   Serial.println();
-   isort(rangevalue, arraysize);
+   isort(rangevalue[sensor], arraysize);
    Serial.print("sorted ");
-   printArray(rangevalue, arraysize);
+   printArray(rangevalue[sensor], arraysize);
    Serial.println();
     
  // now show the medaian range   
     int midpoint = arraysize/2;    //midpoint of the array is the medain value in a sorted array
       //note that for an array of 5, the midpoint is element 2, as the first element is element 0
      Serial.print("median range value ");
-     Serial.print(rangevalue[midpoint]);
+     Serial.print(rangevalue[sensor][midpoint]);
      Serial.println();  
      Serial.println();  
   
-  delay(200);    //wait a while so you can read the values on the serial monitor
+  delay(10);    //wait a while so you can read the values on the serial monitor
 
-}   //end of loop
-
+}   //end of array reading loop
+} // end of sensor loop
 //*********************************************************************************
 // sort function
 void isort(int *a, int n)
